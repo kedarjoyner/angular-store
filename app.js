b013a3f3b3789729c18b2b60a7f05525fd007112
kedarjoyner <kedarjoyner@gmail.com>
2016-10-21 19:43:39 -0500

    add http-server, remove server.js, add ngRoute to store module

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-// 'store' is a directive of <html>
-var app  =   angular.module('store', []);
+(function(){
+var app = angular.module('store', ['ngRoute']);
+app.controller('StoreController',function() {
+    this.product = gem;
+
+});
+var gem = {
+
+    name: 'Dodecahedron',
+    price: 2.95,
+    description:'. . .',
+    canPurchase: false
+};
+})();
